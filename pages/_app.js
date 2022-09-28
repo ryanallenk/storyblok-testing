55fab9281f2bc1006ecd807bf2d4d20f969f61b0
ryanallenk <ryanallenk@gmail.com>
2022-09-28 16:54:12 -0400

    feature: initial storyblok setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
-
+import {storyblokInit, apiPlugin } from '@storyblok/react';
+storyblokInit({
+  accessToken: process.env.STORYBLOK_PREVIEW_TOKEN,
+  use: [apiPlugin]
+})
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
