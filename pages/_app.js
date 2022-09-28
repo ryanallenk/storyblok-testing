35c0fc233290b37b2f04c49960136eaf68389e57
ryanallenk <ryanallenk@gmail.com>
2022-09-28 16:57:23 -0400

    feature: fetching data for storyblok

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,8 @@ import {storyblokInit, apiPlugin } from '@storyblok/react';
 storyblokInit({
   accessToken: process.env.STORYBLOK_PREVIEW_TOKEN,
   use: [apiPlugin]
-})
+});
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
